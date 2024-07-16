3a070918305e9e5aca4bef3355683e81fc2be28b
readfrefita <readfrefita@gmail.com>
2024-07-16 11:40:55 +0100

    initialisation du projet achat

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity
 @Getter
 @Setter
@@ -24,6 +25,7 @@ public class Stock implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idStock;
+	private String desc;
 	private String libelleStock;
 	private Integer qte;
 	private Integer qteMin;
